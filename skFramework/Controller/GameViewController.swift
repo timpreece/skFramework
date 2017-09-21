349db50b51f29140cbcb3c962dc0903d10551ce9
Tim Preece <TimPreece@tims-imac>
2017-09-21 16:29:41 +0100

    Rebuilt for Swift 4

--- skFramework/Controller/GameViewController.swift
+++ skFramework/Controller/GameViewController.swift
@@ -11,42 +11,42 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            
-            let scene = GameScene(size: CGSize(width: 2048, height: 2048))
-            
-            view.ignoresSiblingOrder = true
-            view.showsFPS = true
-            view.showsNodeCount = true
-            
-            // Present the scene
-            view.presentScene(scene)
-        }
+    if let view = self.view as! SKView? {
+      
+      let scene = GameScene(size: CGSize(width: 2048, height: 2048))
+      
+      view.ignoresSiblingOrder = true
+      view.showsFPS = true
+      view.showsNodeCount = true
+      
+      // Present the scene
+      view.presentScene(scene)
     }
-    
-    override var shouldAutorotate: Bool {
-        return true
+  }
+  
+  override var shouldAutorotate: Bool {
+    return true
+  }
+  
+  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+    if UIDevice.current.userInterfaceIdiom == .phone {
+      return .allButUpsideDown
+    } else {
+      return .all
     }
-    
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-    
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Release any cached data, images, etc that aren't in use.
+  }
+  
+  override var prefersStatusBarHidden: Bool {
+    return true
+  }
 }
 
--- skFramework/View/DebugInfo.swift
+++ skFramework/View/DebugInfo.swift
@@ -13,40 +13,40 @@ import GameplayKit
 // helper class that builds a dictionary of debug information and draw it to an SKNode
 //
 class DebugInfo {
+  
+  fileprivate var data = [String : Any]()
+  fileprivate var layer : SKNode?
+  
+  func set( layer: SKNode ){
+    self.layer = layer
+  }
+  
+  func update( value:Any, forKey:String ){
+    data.updateValue(value, forKey: forKey)
+  }
+  
+  func refresh() {
     
-    fileprivate var data = [String : Any]()
-    fileprivate var layer : SKNode?
-    
-    func set( layer: SKNode ){
-        self.layer = layer
+    guard let layer = self.layer,
+      let view = layer.scene?.view else {
+        return
     }
     
-    func update( value:Any, forKey:String ){
-        data.updateValue(value, forKey: forKey)
-    }
+    layer.removeAllChildren()
     
-    func refresh() {
-        
-        guard let layer = self.layer,
-            let view = layer.scene?.view else {
-                return
-        }
-        
-        layer.removeAllChildren()
-        
-        let x = view.bounds.width / 2
-        var y = view.bounds.height / 2
-        
-        for line in data {
-            let node = SKLabelNode()
-            node.fontSize = 10
-            node.fontName = "Courier"
-            node.fontColor = SKColor.white
-            node.horizontalAlignmentMode = .right
-            node.text = "\(line.value)"
-            y = y - node.frame.height - 1
-            node.position = CGPoint(x: x , y: y)
-            layer.addChild( node )
-        }
+    let x = view.bounds.width / 2
+    var y = view.bounds.height / 2
+    
+    for line in data {
+      let node = SKLabelNode()
+      node.fontSize = 10
+      node.fontName = "Courier"
+      node.fontColor = SKColor.white
+      node.horizontalAlignmentMode = .right
+      node.text = "\(line.value)"
+      y = y - node.frame.height - 1
+      node.position = CGPoint(x: x , y: y)
+      layer.addChild( node )
     }
+  }
 }
